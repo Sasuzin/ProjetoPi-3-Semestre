931877bb03e74591038bc385b5a8d11471ecce9b
Your Name <you@example.com>
2022-04-27 21:38:17 -0300

    Imagens colocadas, cartões feitos e menu colocado

--- projetoPi/resources/views/home.blade.php
+++ projetoPi/resources/views/home.blade.php
@@ -4,9 +4,202 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PI</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    
+    <title>Tabacaria - Ecommerce</title>
 </head>
 <body>
-    
+        <nav class="navbar navbar-light navbar-expand-md bg-light pl-5 pr-5 mb-5">
+            <a href="#" class="navbar-brand">Baco Shop</a>
+            <div class="collapse navbar-collapse">
+                <div class="navbar-nav">
+                    <a href="nav-link">Home</a>
+                    <a href="nav-link">Categorias</a>
+                    <a href="nav-link">Cadastrar</a>
+                </div>
+            </div>
+
+            <a href="#" class="btn btn-sm">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart4" viewBox="0 0 16 16">
+                    <path d="M0 2.5A.5.5 0 0 1 .5 2H2a.5.5 0 0 1 .485.379L2.89 4H14.5a.5.5 0 0 1 .485.621l-1.5 6A.5.5 0 0 1 13 11H4a.5.5 0 0 1-.485-.379L1.61 3H.5a.5.5 0 0 1-.5-.5zM3.14 5l.5 2H5V5H3.14zM6 5v2h2V5H6zm3 0v2h2V5H9zm3 0v2h1.36l.5-2H12zm1.11 3H12v2h.61l.5-2zM11 8H9v2h2V8zM8 8H6v2h2V8zM5 8H3.89l.5 2H5V8zm0 5a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0zm9-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm-2 1a2 2 0 1 1 4 0 2 2 0 0 1-4 0z"/>
+                </svg>  
+            </a>
+        </nav>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-3 mb-3">
+                <div class="card">
+                    <div class="card" style="width: 18rem;">
+                        <img src="Images/baco1.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">Tabaco Amsterdam</p>
+                                <div class="card-body">
+                                    <h6 class="card-title">Produto1</h6>
+                                    <a href="#" class="btn btn-sm btn-secondary">Adicionar</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card" style="width: 18rem;">
+                        <img src="Images/baco2.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">Tabaco Grovy</p>
+                                <div class="card-body">
+                                    <h6 class="card-title">Produto2</h6>
+                                    <a href="#" class="btn btn-sm btn-secondary">Adicionar</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card" style="width: 18rem;">
+                        <img src="Images/baco3.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">Tabaquin</p>
+                                <div class="card-body">
+                                    <h6 class="card-title">Produto3</h6>
+                                    <a href="#" class="btn btn-sm btn-secondary">Adicionar</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card" style="width: 18rem;">
+                        <img src="Images/cuia1.png" class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h6 class="card-title">Produto4</h6>
+                                    <a href="#" class="btn btn-sm btn-secondary">Adicionar</a>
+                                </div>
+                                
+                            </div>
+                        </div>
+
+                        <div class="card" style="width: 18rem;">
+                        <img src="Images/cuia2.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">Tabaco Amsterdam</p>
+                                <div class="card-body">
+                                    <h6 class="card-title">Produto1</h6>
+                                    <a href="#" class="btn btn-sm btn-secondary">Adicionar</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card" style="width: 18rem;">
+                        <img src="Images/cuia3.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">Tabaco Amsterdam</p>
+                                <div class="card-body">
+                                    <h6 class="card-title">Produto1</h6>
+                                    <a href="#" class="btn btn-sm btn-secondary">Adicionar</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card" style="width: 18rem;">
+                        <img src="Images/isqueiro1.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">Tabaco Amsterdam</p>
+                                <div class="card-body">
+                                    <h6 class="card-title">Produto1</h6>
+                                    <a href="#" class="btn btn-sm btn-secondary">Adicionar</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card" style="width: 18rem;">
+                        <img src="Images/isqueiro2.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">Tabaco Amsterdam</p>
+                                <div class="card-body">
+                                    <h6 class="card-title">Produto1</h6>
+                                    <a href="#" class="btn btn-sm btn-secondary">Adicionar</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card" style="width: 18rem;">
+                        <img src="Images/isqueiro3.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">Tabaco Amsterdam</p>
+                                <div class="card-body">
+                                    <h6 class="card-title">Produto1</h6>
+                                    <a href="#" class="btn btn-sm btn-secondary">Adicionar</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card" style="width: 18rem;">
+                        <img src="Images/piteira1.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">Tabaco Amsterdam</p>
+                                <div class="card-body">
+                                    <h6 class="card-title">Produto1</h6>
+                                    <a href="#" class="btn btn-sm btn-secondary">Adicionar</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card" style="width: 18rem;">
+                        <img src="Images/piteira2.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">Tabaco Amsterdam</p>
+                                <div class="card-body">
+                                    <h6 class="card-title">Produto1</h6>
+                                    <a href="#" class="btn btn-sm btn-secondary">Adicionar</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card" style="width: 18rem;">
+                        <img src="Images/piteira3.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">Tabaco Amsterdam</p>
+                                <div class="card-body">
+                                    <h6 class="card-title">Produto1</h6>
+                                    <a href="#" class="btn btn-sm btn-secondary">Adicionar</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card" style="width: 18rem;">
+                        <img src="Images/seda1.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">Tabaco Amsterdam</p>
+                                <div class="card-body">
+                                    <h6 class="card-title">Produto1</h6>
+                                    <a href="#" class="btn btn-sm btn-secondary">Adicionar</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card" style="width: 18rem;">
+                        <img src="Images/seda2.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">Tabaco Amsterdam</p>
+                                <div class="card-body">
+                                    <h6 class="card-title">Produto1</h6>
+                                    <a href="#" class="btn btn-sm btn-secondary">Adicionar</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="card" style="width: 18rem;">
+                        <img src="Images/seda3.png" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">Tabaco Amsterdam</p>
+                                <div class="card-body">
+                                    <h6 class="card-title">Produto1</h6>
+                                    <a href="#" class="btn btn-sm btn-secondary">Adicionar</a>
+                                </div>
+                            </div>
+                        </div>
+                        
+                  </div>
+                </div>
+            </div>
+        </div>
+
+
+        
 </body>
 </html>
